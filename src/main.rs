36edc578dcbdd3790241802ce11aeb696b1feae2
Thierry Berger <contact@thierryberger.com>
2020-05-02 12:35:07 +0200

    read serial port instead of console

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,73 @@ impl Input for InputConsole {
     }
 }
 
+
+use std::io::{Write};
+use std::time::Duration;
+
+use clap::{App, AppSettings, Arg};
+use serialport::prelude::*;
+
+struct InputSerial {
+    serial_buf: Vec<u8>,
+    port: Box<dyn SerialPort>,
+}
+
+impl InputSerial {
+    fn new(port_name: &str, baud_rate: u32) -> Result<Self, String> {
+        
+        let mut settings: SerialPortSettings = Default::default();
+        settings.timeout = Duration::from_millis(10);
+        settings.baud_rate = baud_rate;
+
+        match serialport::open_with_settings(&port_name, &settings) {
+            Ok(mut port) => {
+                Ok(InputSerial{serial_buf: vec![0; 1000], port})
+            },
+            _ => {
+                Err("fuck".to_string())
+            }
+        }
+    }
+}
+
+impl Input for InputSerial {
+    fn read(&mut self) -> Option<bool> {
+        match self.port.read(self.serial_buf.as_mut_slice()) {
+            Ok(t) => Some(true),
+            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => None,
+            Err(e) => None,
+        }
+    }
+}
+
 fn main() {
+    let matches = App::new("Serialport Example - Receive Data")
+    .about("Reads data from a serial port and echoes it to stdout")
+    .setting(AppSettings::DisableVersion)
+    .arg(
+        Arg::with_name("port")
+            .help("The device path to a serial port")
+            .use_delimiter(false)
+            .required(true),
+    )
+    .arg(
+        Arg::with_name("baud")
+            .help("The baud rate to connect at")
+            .use_delimiter(false)
+            .required(true),
+    )
+    .get_matches();
+    let port_name = matches.value_of("port").unwrap();
+    let baud_rate = matches.value_of("baud").unwrap();
+    let mut input : InputSerial;
+    if let Ok(baud_rate) = baud_rate.parse::<u32>() {
+        input = InputSerial::new(port_name, baud_rate).unwrap();
+    } else {
+        eprintln!("Error: Invalid baud rate '{}' specified", baud_rate);
+        ::std::process::exit(1);
+    }
+
     let random = rand::thread_rng();
     let sound_paths = fs::read_dir("assets/ogg/")
         .unwrap()
@@ -71,8 +137,10 @@ fn main() {
         .collect::<Result<Vec<_>, io::Error>>()
         .unwrap();
     let mut player = Player::new(random, sound_paths);
+
     // TODO: Next step 1: physical button to trigger a sound
-    let mut input  = InputConsole::new();
+    //let mut input  = InputConsole::new();
+    
     loop {
         if let Some(read) = input.read() {
             if read {
